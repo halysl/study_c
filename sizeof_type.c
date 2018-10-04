a9cdc677d10d77a5e25b15b54024336d970fd53a
yagami light <halysl0817@gmail.com>
2018-10-04 23:43:40 +0800

    upup

---
+++ sizeof_type.c
@@ -0,0 +1,14 @@
+//
+// Created by light on 2018/10/4.
+//
+
+#include <stdio.h>
+
+int main(){
+    printf("size of int:%lu\n", sizeof(int));
+    printf("size of u int:%lu\n", sizeof(unsigned int));
+    printf("size of float:%lu\n", sizeof(float));
+    printf("size of double:%lu\n", sizeof(double));
+    return 0;
+}
+
