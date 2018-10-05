2f1590de181590c02c84e411109874acd5e5ba21
yagami light <halysl0817@gmail.com>
2018-10-05 10:08:14 +0800

    add type sizeof

--- sizeof_type.c
+++ sizeof_type.c
@@ -5,10 +5,19 @@
 #include <stdio.h>
 
 int main(){
+    printf("size of char:%lu\n", sizeof(char));
+    printf("size of unsigned char:%lu\n", sizeof(unsigned char));
+    printf("size of signed char:%lu\n", sizeof(signed char));
     printf("size of int:%lu\n", sizeof(int));
     printf("size of u int:%lu\n", sizeof(unsigned int));
+    printf("size of short:%lu\n", sizeof(short));
+    printf("size of unsigned short:%lu\n", sizeof(unsigned short));
+    printf("size of long:%lu\n", sizeof(long));
+    printf("size of unsigned long:%lu\n", sizeof(unsigned long));
+
     printf("size of float:%lu\n", sizeof(float));
     printf("size of double:%lu\n", sizeof(double));
+    printf("size of long double:%lu\n", sizeof(long double));
     return 0;
 }
 
